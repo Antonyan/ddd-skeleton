abb06e7316acc0e9fab5db2c16f8bd29b3cf0342
Andriy <adobush@remit.se>
2018-08-07 15:57:02 +0300

    added dotenv ext

--- api/api.php
+++ api/api.php
@@ -9,6 +9,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $routes = include __DIR__ . '/../src/app/config/restRoutes.php';
 
+(new \Dotenv\Dotenv(__DIR__.'/../'))->load();
+
 $request = Request::createFromGlobals();
 
 $response = (new Application($routes))->handle($request)->send();
